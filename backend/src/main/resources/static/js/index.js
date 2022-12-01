734803ed7f486f40f843c307a0fd5b95afb37eb2
Gaven <gaven_yeung@163.com>
2022-12-01 22:41:32 +0800

    HTML前端初稿

--- backend/src/main/resources/static/js/index.js
+++ backend/src/main/resources/static/js/index.js
@@ -17,16 +17,108 @@ function buildUrl(path, src, dest) {
   return url;
 }
 
+const chartByHours = echarts.init(document.getElementById('chartByHours'), 'macarons', {renderer: 'svg'});
+const chartByAirlines = echarts.init(document.getElementById('chartByAirlines'), 'macarons', {renderer: 'svg'});
+
+const option = {
+  legend: {
+    data: ['准点航班数', '延误航班数', '取消航班数', '准点率']
+  },
+  tooltip: {
+    trigger: 'axis',
+  },
+  xAxis: {
+    type: 'category',
+    axisLabel: {fontSize: '0.9em'},
+    data: []
+  },
+  yAxis: [
+    {
+      type: 'value',
+      name: '航班数',
+      nameTextStyle: {fontSize: '0.9em'},
+      axisLabel: {
+        axisLabel: {fontSize: '0.9em'},
+      }
+    },
+    {
+      type: 'value',
+      name: '准点率',
+      nameTextStyle: {fontSize: '0.9em'},
+      splitLine: {show: false},
+      splitArea: {show: false},
+      axisLabel: {
+        axisLabel: {fontSize: '0.9em'},
+        formatter: '{value} %'
+      }
+    }
+  ],
+  series: [
+    {
+      name: '准点率',
+      data: [],
+      type: 'line',
+      yAxisIndex: 1,
+      tooltip: {
+        valueFormatter: function (value) {
+          return value + '%';
+        }
+      },
+      markPoint: {
+        data: [
+          {type: 'max', name: '最优'},
+          {type: 'min', name: '最差'}
+        ]
+      },
+    },
+    {
+      name: '准点航班数',
+      type: 'bar',
+      stack: 'total',
+      tooltip: {
+        valueFormatter: function (value) {
+          return value;
+        }
+      },
+      data: []
+    },
+    {
+      name: '延误航班数',
+      type: 'bar',
+      stack: 'total',
+      tooltip: {
+        valueFormatter: function (value) {
+          return value;
+        }
+      },
+      data: []
+    },
+    {
+      name: '取消航班数',
+      type: 'bar',
+      stack: 'total',
+      tooltip: {
+        valueFormatter: function (value) {
+          return value;
+        }
+      },
+      data: []
+    },
+  ]
+};
+chartByHours.setOption(option);
+chartByAirlines.setOption(option);
+
 jQuery(function ($) {
   function show() {
     let s = $('#source').val();
     let t = $('#destination').val();
 
     countAll(s, t);
-    findTop(s, t);
-    findBottom(s, t);
     countByHour(s, t);
     countByAirlines(s, t);
+    findTop(s, t);
+    findBottom(s, t);
   }
 
   function countAll(src, dest) {
@@ -39,7 +131,13 @@ jQuery(function ($) {
     })
       .then((response) => response.json())
       .then((data) => {
-        $('#total').text(formatNumber(data.total));
+        // $('#total').text(formatNumber(data.total));
+        const countUpTotal = new countUp.CountUp('total', data.total);
+        if (!countUpTotal.error) {
+          countUpTotal.start();
+        } else {
+          console.error(countUpTotal.error);
+        }
         $('#otp').text(formatPercentage(data.otp));
         $('#ontimes').text(formatNumber(data.ontimes));
         $('#rod').text(formatPercentage(data.rod));
@@ -50,19 +148,127 @@ jQuery(function ($) {
   }
 
   function countByAirlines(src, dest) {
-
+    let url = buildUrl('countByAirlines', src, dest)
+    fetch(url, {
+      mode: 'no-cors',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => fillChartDate(data, chartByAirlines));
   }
 
   function countByHour(src, dest) {
+    let url = buildUrl('countByHour', src, dest)
+    fetch(url, {
+      mode: 'no-cors',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => fillChartDate(data, chartByHours));
+  }
 
+  function fillChartDate(data, chartObj) {
+    let category = [];
+    let otps = [];
+    let ontimes = [];
+    let delays = [];
+    let cancellations = [];
+
+    data.forEach(e => {
+      category.push(e.category);
+      ontimes.push(e.ontimes);
+      delays.push(e.delays);
+      cancellations.push(e.cancellations);
+      otps.push((e.otp * 100).toFixed(0));
+    });
+
+    let optionExt = {
+      xAxis: {
+        data: category
+      },
+      series: [
+        {
+          name: '准点率',
+          data: otps,
+        },
+        {
+          name: '准点航班数',
+          data: ontimes
+        },
+        {
+          name: '延误航班数',
+          data: delays
+        },
+        {
+          name: '取消航班数',
+          data: cancellations
+        },
+      ]
+    };
+
+    chartObj.setOption(optionExt);
   }
 
   function findTop(src, dest) {
-
+    let url = buildUrl('findTop', src, dest)
+    fetch(url, {
+      mode: 'no-cors',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let l = data.length;
+        let tbody = $('#top-table > tbody');
+        fillTable(tbody, l, data);
+      });
   }
 
   function findBottom(src, dest) {
+    let url = buildUrl('findBottom', src, dest)
+    fetch(url, {
+      mode: 'no-cors',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let l = data.length;
+        let tbody = $('#bottom-table > tbody');
+        fillTable(tbody, l, data);
+      });
+  }
 
+  function fillTable(tbody, l, data) {
+    for (let i = 1; i < 6; i++) {
+      let th = tbody.children('tr:nth-child(' + i + ')');
+      if (i <= l) {
+        let item = data[i - 1];
+        th.children('td:nth-child(1)').children('div').text(item.category);
+        th.children('td:nth-child(2)').text(formatNumber(item.total));
+        th.children('td:nth-child(3)').text(formatNumber(item.ontimes));
+        th.children('td:nth-child(4)').text(formatNumber(item.delays));
+        th.children('td:nth-child(5)').text(formatNumber(item.cancellations));
+        th.children('td:nth-child(6)').text(formatPercentage(item.otp));
+        th.children('td:nth-child(7)').text(formatPercentage(item.rod));
+        th.children('td:nth-child(8)').text(formatPercentage(item.roc));
+      } else {
+        th.children('td:nth-child(1)').children('div').text('');
+        th.children('td:nth-child(2)').text('');
+        th.children('td:nth-child(3)').text('');
+        th.children('td:nth-child(4)').text('');
+        th.children('td:nth-child(5)').text('');
+        th.children('td:nth-child(6)').text('');
+        th.children('td:nth-child(7)').text('');
+        th.children('td:nth-child(8)').text('');
+      }
+    }
   }
 
   $('#swap').click(
